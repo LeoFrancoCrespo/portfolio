817d6f1096b6ac637ad24e3ed131a5136cc6e82e
Leo <leo.francocrespo@gmail.com>
2018-10-30 21:42:23 -0500

    portfolio.views now handles render to portfolio/home.html

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, this is home")
+    return render(request, 'portfolio/home.html')
